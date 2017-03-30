8b9d6a9f8eb68fdc3b8df593465b6c8cfffa29e9
Matthias Plock <matthias.plock@bam.de>
2017-03-30 13:46:51 +0200

    Added command line argument parsing

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # fem-gl
 ## Display FEM raw data in a modern web browser.
 
-Call `./bare_http_server.py` in a shell. 
-Direct your browser to `localhost:8080`.
+Call `./fem_gl.py` in a shell. 
+Direct your browser to `localhost:8008`.
 
 Mouse drag to rotate the object, mouse drag and CTRL to move the object and 
 mouse wheel to zoom in and out.
--- fem_gl.py
+++ fem_gl.py
@@ -1,5 +1,38 @@
 #!/usr/bin/env python
+"""Start a http server for fem-gl and start serving the html directory.
+"""
 
 import modules.web_service as fem_web
-import modules.mesh_parser as fem_parser
+import argparse
+import os
 
+parser = argparse.ArgumentParser(description='Start a web server for fem-gl. '\
+                                 'Direct your browser to [HOST_IP]:[PORT] '\
+                                 'with PORT being either 8008 or the '\
+                                 'supplied value.')
+parser.add_argument('-p', '--port', default=8008,
+                    help='The port for the web server. Defaults to 8008.')
+parser.add_argument('-m', '--mesh-dir', required=True,
+                    help='The directory in which we want '\
+                    'to look for mesh files.')
+args = parser.parse_args()
+
+cwd = os.getcwd()
+
+html_dir = os.path.abspath('html')
+mesh_dir = os.path.abspath(args.mesh_dir)
+
+port = args.port
+
+start_msg = 'Starting fem-gl http server on port {port_text}\n\n'\
+            'Serving html from directory {html_dir_text}\n'\
+            'Will search for fem data in directory {mesh_dir_text}'\
+            '\n'.format(
+                port_text=port,
+                html_dir_text=html_dir,
+                mesh_dir_text=mesh_dir)
+print(start_msg)
+web_instance = fem_web.WebServer(
+    directory=html_dir,
+    port=port)
+web_instance.start()
