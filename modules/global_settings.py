649906ae229c635a4c38f4edb0c20e1b88e3692b
Matthias Plock <matthias.plock@bam.de>
2017-06-12 17:05:06 +0200

    Modified the web server, now everything via JSON

---
+++ modules/global_settings.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# This file is part of fem-gl.
+#
+# Copyright (C) 2017 Matthias Plock <matthias.plock@bam.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+A list of globally available variables.
+"""
+
+def init():
+    """
+    Initialise the global variables.
+    """
+
+    # A list that contains all the active scenes on the server.
+    global global_sceneList
+    global_sceneList = []
